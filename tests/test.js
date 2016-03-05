ea572c857fa426b48a33306c475cafa8230123ae
Jaro <jaro@nous.lt>
2016-03-05 17:17:06 +0200

    Delete demo test.

--- tests/test.js
+++
@@ -1,8 +0,0 @@
-import {expect} from 'chai';
-
-describe('Demo test', function() {
-    it('should should be equal', function () {
-        let count = 10;
-        expect(count).to.be.equal(10);
-    });
-});
